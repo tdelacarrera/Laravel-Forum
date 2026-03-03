de44f34c5f50c35192022e037817b225f786d42a
tdelacarrera <202549429+tdelacarrera@users.noreply.github.com>
2026-03-03 10:08:20 -0300

    Add logout and profile routes in forum navbar

--- resources/views/forum/partials/navbar.blade.php
+++ resources/views/forum/partials/navbar.blade.php
@@ -4,8 +4,13 @@
         <a href="{{ route('register') }}">Register</a>
         <a href="{{ route('login') }}">Login</a>
     @else
-        <a href="">Profile</a>
-        <a href="">Logout</a>
+        <a href="{{ route('profile.edit') }}">Profile</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:inline;">
+            @csrf
+            <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                Logout
+            </a>
+        </form>
     @endguest
     <a href="">Search</a>
     <a href="">Memberlist</a>
